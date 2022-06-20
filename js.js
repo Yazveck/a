b8043197ed13f7c2eb19ce639f2098ef70aa7dcc
Yazveck <106010573+Yazveck@users.noreply.github.com>
2022-06-20 01:48:43 -0500

    Create js.js

---
+++ js.js
@@ -0,0 +1,17 @@
+function mueveElBoton(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    newWidth = (Math.random() * width);
+    newHeight = (Math.random() * height);
+
+    document.getElementById('btnNo').style.position = "absolute";
+    document.getElementById('btnNo').style.left = newWidth + "px";
+    document.getElementById('btnNo').style.top = newHeight + "px";
+    
+
+}
+  function dijoSi(){
+      alert("Gracias por todo <3");
+      
+  }
